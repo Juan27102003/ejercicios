54e0dbcfddca8766b9ac798cfd38275ccfe66749
juan <jgarmar2710@g.educaand.es>
2023-10-05 09:32:03 +0200

    cambio

--- src/T1/Calculadora.java
+++ src/T1/Calculadora.java
@@ -2,16 +2,16 @@ package T1;
 
 import java.util.Scanner;
 
-public class Calculadora {
+public class Calculadora{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double a = sc.nextDouble();
-        double b = sc.nextDouble();
+            Scanner sc = new Scanner(System.in);
+            double a = sc.nextDouble();
+            double b = sc.nextDouble();
 
-        System.out.println("Suma:" + suma(a, b));
-        System.out.println("Resta:" + resta(a, b));
-        System.out.println("Multiplicacion:" + multiplicacion(a, b));
-        System.out.println("Division:" + division(a, b));
+            System.out.println("Suma:" + suma(a, b));
+            System.out.println("Resta:" + resta(a, b));
+            System.out.println("Multiplicacion:" + multiplicacion(a, b));
+            System.out.println("Division:" + division(a, b));
     }
 
     public static double suma(double x, double y) {
@@ -29,4 +29,5 @@ public class Calculadora {
     public static double division(double x, double y) {
         return x / y;
     }
+    
 }
