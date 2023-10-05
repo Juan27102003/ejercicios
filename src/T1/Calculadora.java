816b32772756aef9b9af851877e2a513b3a96198
juan <jgarmar2710@g.educaand.es>
2023-10-05 08:28:34 +0200

    cambio

---
+++ src/T1/Calculadora.java
@@ -0,0 +1,21 @@
+package T1;
+
+import java.util.Scanner;
+
+public class Calculadora {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        double suma = a + b;
+        double resta = a - b;
+        double multiplicacion = a * b;
+        double division = a / b;
+
+        System.out.println("Suma:" + suma);
+        System.out.println("Resta:" + resta);
+        System.out.println("Multiplicacion:" + multiplicacion);
+        System.out.println("Division:" + division);
+    }
+}
