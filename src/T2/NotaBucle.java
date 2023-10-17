ed08775b30126806fd9a600d1d09dd35747c419b
juan <jgarmar2710@g.educaand.es>
2023-10-17 09:34:46 +0200

    cambio

---
+++ src/T2/NotaBucle.java
@@ -0,0 +1,19 @@
+package T2;
+
+import java.util.Scanner;
+
+public class NotaBucle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        float nota=0;
+        float suma=0;
+        while (nota!=-1){
+            System.out.println("Introduce nota:");
+            nota=sc.nextFloat();
+            if (nota!=-1){
+                suma+=nota;
+            }
+        }
+        System.out.println(suma);
+    }
+}
