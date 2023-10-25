0387bfeb6dfaaea28c4f46fdf03b08d5adba9a3a
juan <jgarmar2710@g.educaand.es>
2023-10-25 09:39:38 +0200

    cambio

--- src/T2/RellenandoConPuntos.java
+++ src/T2/RellenandoConPuntos.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class RellenandoConPuntos {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int n,aux=1;
+        int n,aux=1,v;
         String cad;
         char caracter;
         do {
@@ -16,6 +16,7 @@ public class RellenandoConPuntos {
                 aux=0;
             }
         }while (aux!=0);
+        v=cad.length();
         System.out.println("Introduce el numero de caracteres:");
         n= sc.nextInt();
         do {
@@ -23,26 +24,31 @@ public class RellenandoConPuntos {
             caracter=sc.next().charAt(0);
             if (caracter=='I'){
                 for (int i=0;i<=n;i++){
-                    if (n>cad.length()){
-                        cad="." + cad;
-                    }else if (n==cad.length()){
-                        System.out.println("La cadena no cabe");
+                    if (n>cad.length()) {
+                        cad = "." + cad;
                     }
+                }
+                if(n==v||n<v){
+                    System.out.println("La cadena no cabe");
                 }
                 aux=1;
             } else if (caracter=='D') {
                 for (int i=0;i<=n;i++){
                     if (n>cad.length()){
                         cad=cad + ".";
-                    }else if (n==cad.length()){
-                        System.out.println("La cadena no cabe");
                     }
                 }
+                if(n==v||n<v){
+                    System.out.println("La cadena no cabe");
+                }
                 aux=1;
+
             }else {
                 System.out.println("El caracter introducido no es valido");
             }
         }while (aux!=1);
-        System.out.println(cad);
+        if (n>v){
+            System.out.println(cad);
+        }
     }
 }
