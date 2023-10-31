f5e9ca3e349a7dbbaae44f6481e898614da0b13a
juan <jgarmar2710@g.educaand.es>
2023-10-31 08:25:44 +0100

    cambio

--- src/T2/DibujarPiramide.java
+++ src/T2/DibujarPiramide.java
@@ -8,7 +8,7 @@ public class DibujarPiramide {
         int altura = sc.nextInt();
         for (int i = 1; i <= altura; i++) {
             for (int j = 1; j <= altura - i; j++) {
-                System.out.print("-");
+                System.out.print(" ");
             }
             for (int k = 1; k < 2 * i; k++) {
                 System.out.print("*");
